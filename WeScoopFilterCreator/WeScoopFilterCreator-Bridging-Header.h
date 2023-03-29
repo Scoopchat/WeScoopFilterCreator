83d06b680615a0d40a7d0f671d2ae3e831f68639
bohonskayats <84713455+bohonskayats@users.noreply.github.com>
2023-03-29 10:08:57 +0200

    Initial Commit

---
+++ WeScoopFilterCreator/WeScoopFilterCreator-Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  WeScoopFilterCreator-Bridging-Header.h
+//  WeScoopFilterCreator
+//
+//  Created by Dorian on 29.03.2023.
+//
+
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "ShaderTypes.h"
